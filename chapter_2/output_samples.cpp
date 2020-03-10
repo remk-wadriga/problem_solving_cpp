cda6a3bdf8b3e06da3293c8e69eece4a983fbe7f
rkwadiga <rkwadiga@gmail.com>
2020-03-10 13:57:08 +0200

    Chapter 2. Add all possible variants of figures viewing for tasks "print the half of squere" and "print isosceles triangle"

--- chapter_2/output_samples.cpp
+++ chapter_2/output_samples.cpp
@@ -1,8 +1,9 @@
-// Prints something like this:
-// ###
-// ##
-// #
-void print_half_of_square(int size)
+/* Prints something like this:
+* ###
+* ##
+* #
+*/
+void print_half_of_square_right_1(int size)
 {
     using namespace std;
     for (int row = size; row > 0; row--) {
@@ -13,13 +14,69 @@ void print_half_of_square(int size)
     }
 }
 
-// Prints something like this:
-// #
-// ##
-// ###
-// ##
-// #
-void print_isosceles_triangle(int size)
+/* Prints something like this:
+* ###
+*  ##
+*   #
+*/
+void print_half_of_square_left_1(int size)
+{
+    using namespace std;
+    for (int row = 0; row < size; row++) {
+    	for (int spaces = 0; spaces < row; spaces++) {
+    		cout << " ";
+    	}
+    	for (int grids = 0; grids < size - row; grids++) {
+            cout << "#";
+        }
+    	cout << "\n";
+    }
+}
+
+/* Prints something like this:
+* #
+* ##
+* ###
+*/
+void print_half_of_square_right_2(int size)
+{
+    using namespace std;
+    for (int row = 1; row <= size; row++) {
+    	for (int line = 0; line < row; line++) {
+    		cout << "#";
+    	}
+    	cout << "\n";
+    }
+}
+
+/* Prints something like this:
+*   #
+*  ##
+* ###
+*/
+void print_half_of_square_left_2(int size)
+{
+    using namespace std;
+    for (int row = 1; row <= size; row++) {
+    	for (int spaces = 0; spaces < size - row; spaces++) {
+            cout << " ";
+        }
+        for (int grids = 0; grids < row; grids++) {
+            cout << "#";
+        }
+        cout << "\n";
+    }
+}
+
+
+/** Prints something like this:
+* #
+* ##
+* ###
+* ##
+* #
+*/
+void print_triangle_right(int size)
 {
     using namespace std;
     int double_size = size*2;
@@ -34,3 +91,73 @@ void print_isosceles_triangle(int size)
     }
 }
 
+/** Prints something like this:
+*   #
+*  ##
+* ###
+*  ##
+*   #
+*/
+void print_triangle_left(int size)
+{
+    using namespace std;
+    int double_size = size * 2;
+    for (int row = 1; row < double_size; row++) {
+    	int spaces_count = abs(size - row);
+        int grids_count = size - spaces_count;
+        for (int spaces = 0; spaces < spaces_count; spaces++) {
+            cout << " ";
+        }
+        for (int grids = 0; grids < grids_count; grids++) {
+            cout << "#";
+        }
+        cout << "\n";
+    }
+}
+
+/** Prints something like this:
+*    #
+*   ###
+*  #####
+*/
+void print_triangle_top(int size)
+{
+    using namespace std;
+    int rest = size % 2;
+    int height = size / 2 + rest;
+
+    for (int row = 1; row <= height; row++) {
+    	int spaces_count = height - row;
+        int grids_count = row * 2 - rest;
+        for (int spaces = 0; spaces < spaces_count; spaces++) {
+            cout << " ";
+        }
+        for (int grids = 0; grids < grids_count; grids++) {
+            cout << "#";
+        }
+        cout << "\n";
+    }
+}
+
+/** Prints something like this:
+*  #####
+*   ###
+*    #
+*/
+void print_triangle_down(int size)
+{
+    using namespace std;
+    int height = size / 2 + size % 2;
+
+    for (int row = 0; row < height; row++) {
+        int spaces_count = row;
+        int grids_count = size - row * 2;
+        for (int spaces = 0; spaces < spaces_count; spaces++) {
+            cout << " ";
+        }
+        for (int grids = 0; grids < grids_count; grids++) {
+            cout << "#";
+        }
+        cout << "\n";
+    }
+}
--- main.cpp
+++ main.cpp
@@ -6,8 +6,15 @@ int main()
 {
     using namespace std;
 
-    //print_half_of_square(5);
-    print_isosceles_triangle(5);
+    //print_half_of_square_right_1(5);
+    //print_half_of_square_left_1(5);
+    //print_half_of_square_right_2(5);
+    //print_half_of_square_left_2(5);
+
+    //print_triangle_right(5);
+    //print_triangle_left(5);
+    //print_triangle_top(5);
+    print_triangle_down(5);
 
     return 0;
 }
