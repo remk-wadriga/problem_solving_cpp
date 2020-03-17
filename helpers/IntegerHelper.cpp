0960b8e25230f9ff7b4917c88d3515b5e0db0445
rkwadiga <rkwadiga@gmail.com>
2020-03-17 11:35:42 +0200

    Chapter 2. dec2bin, dec2hex, bin2dec and hex2dec functions.

--- helpers/IntegerHelper.cpp
+++ helpers/IntegerHelper.cpp
@@ -16,3 +16,120 @@ int stringToInteger(std::string code)
     }
     return result;
 }
+
+/**
+* 4 => 100
+* 15 => 1111
+* 124 => 1111100
+* 256 => 100000000
+* 2345678 => 1000111100101011001110
+* 87342345435656 => 10011110110111111111001011110010011101000001000
+*/
+std::string dec2bin (long dec)
+{
+    using namespace std;
+    string bin = "";
+    long rest = 0;
+
+    while (dec > 0) {
+        rest = dec % 2;
+        dec /= 2;
+        bin = to_string(rest) + bin;
+    }
+
+    return bin;
+}
+
+/**
+* 4 => 4
+* 15 => f
+* 124 => 7c
+* 256 => 100
+* 2345678 => 23cace
+* 87342345435656 => 4f6ff9793a08
+*/
+std::string dec2hex (long dec)
+{
+    using namespace std;
+    string hex = "";
+    long rest = 0;
+    string intVal;
+
+    while (dec > 0) {
+        rest = dec % 16;
+        dec /= 16;
+        switch (rest) {
+            case 10:
+                intVal = "A";
+                break;
+            case 11:
+                intVal = "B";
+                break;
+            case 12:
+                intVal = "C";
+                break;
+            case 13:
+                intVal = "D";
+                break;
+            case 14:
+                intVal = "E";
+                break;
+            case 15:
+                intVal = "F";
+                break;
+            default:
+                intVal = to_string(rest);
+                break;
+        }
+
+        hex = intVal + hex;
+    }
+
+    return hex;
+}
+
+/**
+* 100 => 4
+* 1111 => 15
+* 1111100 => 124
+* 100000000 => 256
+* 1000111100101011001110 => 2345678
+* 10011110110111111111001011110010011101000001000 => 87342345435656
+*/
+long bin2dec (std::string bin)
+{
+    using namespace std;
+    long dec = 0;
+    int length = bin.length();
+    int intVal = 0;
+
+    for (int i = 0; i < length; i++) {
+    	intVal = bin[i] - '0';
+    	dec = dec * 2 + intVal;
+    }
+
+    return dec;
+}
+
+/**
+* 4 => 4
+* f => 15
+* 7c => 124
+* 100 => 256
+* 23cace => 2345678
+* 4f6ff9793a08 => 87342345435656
+*/
+long hex2dec (std::string hex)
+{
+    using namespace std;
+    long dec = 0;
+    int length = hex.length();
+    int intVal = 0;
+
+    for (int i = 0; i < length; i++) {
+        intVal = (hex[i] >= 'A') ? (hex[i] >= 'a') ? (hex[i] - 'a' + 10) : (hex[i] - 'A' + 10) : (hex[i] - '0');
+        dec = dec * 16 + intVal;
+    }
+
+    return dec;
+}
--- main.cpp
+++ main.cpp
@@ -8,11 +8,20 @@ int main()
 {
     using namespace std;
 
-    for (int i = 3; i <= 9; i++) {
-        cout << i << "\n";
-    	printFatX(i);
-        cout << "\n";
-    }
+    /*long dec1 = 87342345435656;
+    cout << "dec2bin: " << dec1 << " => " << dec2bin(dec1) << "\n";
+    cout << "\n";*/
+
+    long dec2 = 87342345435656;
+    cout << "dec2hex: " << dec2 << " => " << dec2hex(dec2) << "\n";
+
+    /*string bin = "10011110110111111111001011110010011101000001000";
+    cout << "bin2dec: " << bin << " => " << bin2dec(bin) << "\n";
+    cout << "\n";*/
+
+    /*string hex = "4f6ff9793a08";
+    cout << "hex2dec: " << hex << " => " << hex2dec(hex) << "\n";
+    cout << "\n";*/
 
     return 0;
 }
