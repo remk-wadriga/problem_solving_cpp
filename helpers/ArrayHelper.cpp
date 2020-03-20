00e5f89aad5a21401e996b4a02c256a90fa5bd49
rkwadiga <rkwadiga@gmail.com>
2020-03-20 16:46:16 +0200

    Chapter 3. Disable iteration calculation for all algorithms and save algorithms working results for C++ and PHP.

--- helpers/ArrayHelper.cpp
+++ helpers/ArrayHelper.cpp
@@ -31,42 +31,17 @@ void printArr(int arr[], const int size)
     cout << "]";
 }
 
-void selectionSort(int arr[], const int size)
-{
-    using namespace std;
-    int val, check, nextIndex, minIndex;
-    int maxIndex = size - 1;
-    long long steps = 0;
-
-    for (int j = 0; j < maxIndex; j++) {
-        minIndex = j;
-        for (int i = j + 1; i < size; i++) {
-            steps++;
-            if (arr[i] < arr[minIndex]) {
-                minIndex = i;
-            }
-        }
-        if (minIndex != j) {
-            val = arr[minIndex];
-            arr[minIndex] = arr[j];
-            arr[j] = val;
-        }
-    }
-
-    cout << steps << "\n";
-}
+// <-- Sorting algorithms -->
 
 void bubbleSort(int arr[], const int size)
 {
     using namespace std;
     int nextVal, check, nextIndex, maxIndex;
-    long long steps = 0;
 
     for (int j = 1; j < size; j++) {
         check = 0;
         maxIndex = size - j;
         for (int i = 0; i < maxIndex; i++) {
-            steps++;
         	nextIndex = i + 1;
         	nextVal = arr[nextIndex];
         	if (arr[i] > nextVal) {
@@ -79,20 +54,16 @@ void bubbleSort(int arr[], const int size)
             break;
         }
     }
-
-    cout << steps << "\n";
 }
 
 void insertionSort(int arr[], const int size)
 {
     using namespace std;
     int val, prevIndex, prevVal;
-    long long steps = 0;
 
     for (int j = 1; j < size; j++) {
         val = arr[j];
     	for (int i = j; i > 0; i--) {
-    	    steps++;
     	    prevIndex = i - 1;
     	    prevVal = arr[prevIndex];
     	    if (val >= prevVal) {
@@ -102,6 +73,50 @@ void insertionSort(int arr[], const int size)
     	    arr[i] = prevVal;
     	}
     }
-
-    cout << steps << "\n";
 }
+
+// Selection results:
+    // C++:
+        // With iteration calculation:
+            // Bubbles.     10: 45 (0.000823), 100: 4914 (0.000841), 1000: 497609 (0.003192), 10000: 49994880 (0.297736), 100000: 4999876847 (33.7419), 1000000: 499999477209 (3426.62)
+            // Inserts.     10: 34 (0.000821), 100: 2574 (0.000833), 1000: 253770 (0.002617), 10000: 24921357 (0.173482), 100000: 2493902983 (17.3207), 1000000: 249836674360 (1747.01)
+            // Selection.   10: 45 (0.000828), 100: 4950 (0.000838), 1000: 499500 (0.001691), 10000: 49995000 (0.082824), 100000: 4999950000 (8.21922), 1000000: 499999500000 (857.421)
+        // Without iteration calculation:
+            // Bubbles.     10: 45 (0.000808), 100: 4914 (0.001138), 1000: 497609 (0.014371), 10000: 49994880 (0.306641), 100000: 4999876847 (34.1137), 1000000: 499999477209 (3465.76)
+            // Inserts.     10: 34 (0.000878), 100: 2574 (0.001064), 1000: 253770 (0.006196), 10000: 24921357 (0.171522), 100000: 2493902983 (16.9245), 1000000: 249836674360 (1696.16)
+            // Selection.   10: 45 (0.000986), 100: 4950 (0.000995), 1000: 499500 (0.006226), 10000: 49995000 (0.095727), 100000: 4999950000 (8.98968), 1000000: 499999500000 (898.247)
+
+
+    // PHP:
+        // With iteration calculation:
+            // Bubbles.     10: 45 (0.000023), 100: 4950 (0.000657), 1000: 499499 (0.059985), 10000: 49995000 (6.070219), 100000: 4999949994 (627.978346)
+            // Inserts.     10: 21 (0.000006), 100: 2174 (0.000276), 1000: 245271 (0.027567), 10000: 24767003 (2.739803), 100000: 2501300562 (278.207454)
+            // Selection.   10: 45 (0.000020), 100: 4950 (0.000253), 1000: 499500 (0.021427), 10000: 49995000 (2.084112), 100000: 4999950000 (206.039659)
+        // Without iteration calculation:
+            // Bubbles.     10: 45 (0.000021), 100: 4950 (0.000511), 1000: 499499 (0.049860), 10000: 49995000 (5.009871), 100000: 4999949994 (500.853912)
+            // Inserts.     10: 21 (0.000020), 100: 2174 (0.000232), 1000: 245271 (0.022502), 10000: 24767003 (2.247104), 100000: 2501300562 (225.239058)
+            // Selection.   10: 45 (0.000019), 100: 4950 (0.000143), 1000: 499500 (0.010860), 10000: 49995000 (1.008008), 100000: 4999950000 (100.108101)
+
+void selectionSort(int arr[], const int size)
+{
+    using namespace std;
+    int val, minValPos, minVal;
+    int maxIndex = size - 1;
+
+    for (int j = 0; j < maxIndex; j++) {
+        minValPos = j;
+        minVal = arr[minValPos];
+        for (int i = j + 1; i < size; i++) {
+            if (arr[i] < minVal) {
+                minValPos = i;
+                minVal = arr[i];
+            }
+        }
+        if (minValPos != j) {
+            arr[minValPos] = arr[j];
+            arr[j] = minVal;
+        }
+    }
+}
+
+// <-- END Sorting algorithms -->
--- main.cpp
+++ main.cpp
@@ -8,21 +8,15 @@ int main()
 {
     using namespace std;
 
-    const int size = 1000000;
+    int size;
+    cout << "Set array size:\n> ";
+    cin >> size;
     const int ARRAY_SIZE = size;
     int* arr = generateArr(ARRAY_SIZE, -size, size);
 
     srand(time(0));
+    
 
-    selectionSort(arr, ARRAY_SIZE); // 10 -> 45 (0.000828); 100 -> 4950 (0.000838); 1000 -> 499500 (0.001691); 10000 -> 49995000 (0.082824); 100000 -> 4999950000 (8.21922); 1000000 -> 499999500000 (857.421)
-    //printArr(arr, ARRAY_SIZE);
-
-    //bubbleSort(arr, ARRAY_SIZE); // 10 -> 45 (0.000823); 100 -> 4914 (0.000841); 1000 -> 497609 (0.003192); 10000 -> 49994880 (0.297736); 100000 -> 4999876847 (33.7419); 1000000 -> ()
-    //printArr(arr, ARRAY_SIZE);
-
-    //insertionSort(arr, ARRAY_SIZE); // 10 -> 34 (0.000821); 100 -> 2574 (0.000833); 1000 -> 253770 (0.002617); 10000 -> 24921357 (0.173482); 100000 -> 2493902983 (17.3207); 1000000 -> 249836674360 (1747.01)
-    //printArr(arr, ARRAY_SIZE);
-    //cout << "\n";
 
     cout << "Runtime: " << clock() / 1000000.0 << "\n";
 
