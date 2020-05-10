e8f39f41494d0454400761b9ec7971c75e351a41
rkwadiga <rkwadiga@gmail.com>
2020-05-10 13:34:38 +0300

    Chapter 6. Add functions "calculatePositiveIntegersSumRecursive" and "isEvenOneBytesCountRecursive" to Chapter_6/IntegerListFunctions.cpp (tasks 6.1 and 6.2).

--- Chapter_6/IntegerListFunctions.cpp
+++ Chapter_6/IntegerListFunctions.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
-#include "./IntegerList.cpp"
 
 int calculateNegativeIntegers(IntegerList* list)
 {
     int count = 0;
-    AbstractListItem* item;
+    /*AbstractListItem* item;
     while ((item = list->getNextItem()) != NULL) {
         if (((ListNode*)item)->data < 0) {
             count++;
         }
-    }
+    }*/
     return count;
 }
 
 int calculateNegativeIntegersRecursive(IntegerList* list)
 {
-    AbstractListItem* item = list->getNextItem();
+    /*AbstractListItem* item = list->getNextItem();
     if (item == NULL) {
         return 0;
     }
-    return calculateNegativeIntegersRecursive(list) + (int)(((ListNode*)item)->data < 0);
+    return calculateNegativeIntegersRecursive(list) + (int)(((ListNode*)item)->data < 0);*/
+    return 0;
+}
+
+int calculatePositiveIntegersSumRecursive(int* arr, int size)
+{
+    // Exit condition
+    if (size <= 0) {
+        return 0;
+    }
+    // Recursion
+    return calculatePositiveIntegersSumRecursive(arr, size - 1) + (arr[size - 1] > 0 ? arr[size - 1] : 0);
+}
+
+bool isEvenOneBytesCountRecursive(int* arr, int size)
+{
+    // Exit condition
+    if (size <= 0) {
+        return 0;
+    }
+    // Recursion
+    return (isEvenOneBytesCountRecursive(arr, size - 1) + arr[size - 1]) % 2 == 0;
 }
 
 void demonstrateIntegersList()
@@ -34,6 +54,12 @@ void demonstrateIntegersList()
     list1.add(-5);
     list1.add(0);
 
-    list1.print();
-    cout << calculateNegativeIntegersRecursive(&list1) << "\n";
+    /*list1.print();
+    cout << calculateNegativeIntegersRecursive(&list1) << "\n";*/
+
+    const int ARRAY_SIZE = 5;
+    //int* arr = generateArr(ARRAY_SIZE, 0, 1);
+    int arr[ARRAY_SIZE] = {0, 1, 0, 1, 0};
+    printArr(arr, ARRAY_SIZE, " ", false);
+    cout << isEvenOneBytesCountRecursive(arr, ARRAY_SIZE) << "\n";
 }
